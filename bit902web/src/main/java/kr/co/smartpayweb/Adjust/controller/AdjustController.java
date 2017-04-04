ebbd637c1763460e58f3f71a27edd70fbd2365b2
uruga00 <uruga00@gmail.com>
2017-04-04 12:41:13 +0900

    하하

--- bit902web/src/main/java/kr/co/smartpayweb/Adjust/controller/AdjustController.java
+++ bit902web/src/main/java/kr/co/smartpayweb/Adjust/controller/AdjustController.java
@@ -1,7 +1,6 @@
 package kr.co.smartpayweb.Adjust.controller;
 
 import java.io.IOException;
-import java.util.Date;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -9,6 +8,7 @@ import javax.servlet.ServletException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import kr.co.smartpayweb.Adjust.service.AdjustService;
 import kr.co.smartpayweb.repository.vo.dayAdjustVO;
@@ -20,10 +20,11 @@ public class AdjustController {
 	AdjustService adjustService;
 	
 	@RequestMapping("/dayAdjust.do")
-	public List<dayAdjustVO> retrieveDayAdjust() throws ServletException, IOException {
+	public ModelAndView retrieveDayAdjust() throws ServletException, IOException {
 		
 		List<dayAdjustVO> retrieveDayAdjust = adjustService.retrieveDayAdjust();
-		return retrieveDayAdjust;
+		ModelAndView mav = new ModelAndView("adjust/adjust");
+		mav.addObject("retrieveDayAdjust", retrieveDayAdjust);
+		return mav;
 	}
-	
 }
