6b8f53ce1fb902507c1014bc9b9e6062deac4a96
PARK\박용화 <sccv7777@naver.com>
2017-04-20 12:41:25 +0900

    이벤트 VO 올립니다

---
+++ bit902web/src/main/java/kr/co/smartpayweb/repository/vo/EventVO.java
@@ -0,0 +1,27 @@
+package kr.co.smartpayweb.repository.vo;
+
+public class EventVO {
+
+	private int eventNo;
+	private String eventContent;
+	private int storeNo;
+	
+	public int getEventNo() {
+		return eventNo;
+	}
+	public void setEventNo(int eventNo) {
+		this.eventNo = eventNo;
+	}
+	public String getEventContent() {
+		return eventContent;
+	}
+	public void setEventContent(String eventContent) {
+		this.eventContent = eventContent;
+	}
+	public int getStoreNo() {
+		return storeNo;
+	}
+	public void setStoreNo(int storeNo) {
+		this.storeNo = storeNo;
+	}
+}
