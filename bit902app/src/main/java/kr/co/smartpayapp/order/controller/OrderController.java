74a32ae439952e3c19247e45a62814780e385576
uruga00 <uruga00@gmail.com>
2017-04-30 17:56:35 +0900

    노드

--- bit902app/src/main/java/kr/co/smartpayapp/order/controller/OrderController.java
+++ bit902app/src/main/java/kr/co/smartpayapp/order/controller/OrderController.java
@@ -2,7 +2,6 @@ package kr.co.smartpayapp.order.controller;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -10,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import kr.co.smartpayapp.order.service.OrderService;
 
@@ -23,13 +23,11 @@ public class OrderController {
 	
 	@CrossOrigin(origins = "*")
 	@RequestMapping(value = "/regist.do", method = RequestMethod.POST)
-	public Map<String,Object> orderRegist(@RequestBody ArrayList<HashMap<String,String>> order) throws Exception {
+	public ModelAndView orderRegist(@RequestBody ArrayList<HashMap<String,String>> order) throws Exception {
 		
 		orderService.registOrder(order);
 		
-		Map<String,Object> msg = new HashMap<>();
-		msg.put("msg", "주문완료");
-		
-		return msg;
+		ModelAndView mav = new ModelAndView("bit902node/alert/alert.js");
+		return mav;
 	}
 }
