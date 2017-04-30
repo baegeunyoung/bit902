abe8d53b14a78206e569f9927c6fcea80b416449
uruga00 <uruga00@gmail.com>
2017-04-30 18:44:49 +0900

    노드

--- bit902app/src/main/java/kr/co/smartpayapp/order/controller/OrderController.java
+++ bit902app/src/main/java/kr/co/smartpayapp/order/controller/OrderController.java
@@ -27,7 +27,7 @@ public class OrderController {
 		
 		orderService.registOrder(order);
 		
-		ModelAndView mav = new ModelAndView("/bit902node/alert/server.js");
+		ModelAndView mav = new ModelAndView("http://14.32.66.123:8888/bit902node/alert/server.js");
 		return mav;
 	}
 }
