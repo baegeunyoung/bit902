7c9114ed4df56f52d34566d4a4c9518e07819d3d
uruga00 <uruga00@gmail.com>
2017-05-02 17:14:55 +0900

    노드

--- bit902node/alert/server.js
+++ bit902node/alert/server.js
@@ -1,19 +1,63 @@
-var http = require('http');
-var fs = require('fs');
+var app = require('express')();
+var server = require('http').createServer(app);
+// http server를 socket.io server로 upgrade한다
+var io = require('socket.io')(server);
 
-function send404Response(response) {
-	response.writeHead(404, {"Content-Type" : "text/plain"});
-	response.write("404 Error : oppps!!!");
-	response.end();
-}
+// localhost:8888으로 서버에 접속하면 클라이언트로 index.html을 전송한다
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
 
-function onRequest(request, response) {
-	if(request.method == 'GET' && request.url == '/') {
-		response.writeHead(200, {"Content-Type" : "text/html"});
-		fs.createReadStream("./index.html").pipe(response);
-	} else {
-		send404Response(response);
-	}
-}
-	http.createServer(onRequest).listen(8888);
-	console.log("서버가 돕니다");	
+// connection event handler
+// connection이 수립되면 event handler function의 인자로 socket인 들어온다
+io.on('connection', function(socket) {
+
+  // 접속한 클라이언트의 정보가 수신되면
+  socket.on('login', function(data) {
+    console.log('Client logged-in:\n name:' + data.name + '\n userid: ' + data.userid);
+
+    // socket에 클라이언트 정보를 저장한다
+    socket.name = data.name;
+    socket.userid = data.userid;
+
+    // 접속된 모든 클라이언트에게 메시지를 전송한다
+    io.emit('login', data.name );
+  });
+
+  // 클라이언트로부터의 메시지가 수신되면
+  socket.on('chat', function(data) {
+    console.log('Message from %s: %s', socket.name, data.msg);
+
+    var msg = {
+      from: {
+        userid: socket.userid
+      },
+      msg: data.msg
+    };
+
+    // 메시지를 전송한 클라이언트를 제외한 모든 클라이언트에게 메시지를 전송한다
+    socket.broadcast.emit('chat', msg);
+
+    // 메시지를 전송한 클라이언트에게만 메시지를 전송한다
+    // socket.emit('s2c chat', msg);
+
+    // 접속된 모든 클라이언트에게 메시지를 전송한다
+    // io.emit('s2c chat', msg);
+
+    // 특정 클라이언트에게만 메시지를 전송한다
+    // io.to(id).emit('s2c chat', data);
+  });
+
+  // force client disconnect from server
+  socket.on('forceDisconnect', function() {
+    socket.disconnect();
+  })
+
+  socket.on('disconnect', function() {
+    console.log('user disconnected: ' + socket.name);
+  });
+});
+
+server.listen(8888, function() {
+  console.log('Socket IO server listening on port 8888');
+});
