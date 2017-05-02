c898df6799fe8b26d5104c20f6c74f03d0a89d28
uruga00 <uruga00@gmail.com>
2017-05-02 19:02:27 +0900

    ㅇ

--- bit902node/alert/server.js
+++ bit902node/alert/server.js
@@ -1,63 +1,29 @@
-var app = require('express')();
-var server = require('http').createServer(app);
-// http server를 socket.io server로 upgrade한다
-var io = require('socket.io')(server);
+var http = require("http");
+var fs   = require("fs");
+var io   = require("socket.io");
 
-// localhost:8888로 서버에 접속하면 클라이언트로 index.html을 전송한다
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+var server = http.createServer(function(request, response) {
+  
+})
+.listen(10001, function () {
+  console.log("서버 구동 시작...");
 });
 
-// connection event handler
-// connection이 수립되면 event handler function의 인자로 socket인 들어온다
-io.on('connection', function(socket) {
-
-  // 접속한 클라이언트의 정보가 수신되면
-  socket.on('login', function(data) {
-    console.log('Client logged-in:\n name:' + data.name + '\n userid: ' + data.userid);
-
-    // socket에 클라이언트 정보를 저장한다
-    socket.name = data.name;
-    socket.userid = data.userid;
-
-    // 접속된 모든 클라이언트에게 메시지를 전송한다
-    io.emit('login', data.name );
+// 소켓 IO 객체 생성 및 구동
+var socketIo = io.listen(server);
+var idArr = [];
+socketIo.on("connection", function (socket) {
+  
+  // 마지막 접속한 사용자의 소켓 구분 아이디를 저장
+  socket.on("login", function (loginId) {
+    console.log("접속한 회원 정보 : " + loginId, socket.id);
+    // 입력한 아이디와 socket.id를 연결
+    idArr[loginId] = socket.id;
   });
-
-  // 클라이언트로부터의 메시지가 수신되면
-  socket.on('chat', function(data) {
-    console.log('Message from %s: %s', socket.name, data.msg);
-
-    var msg = {
-      from: {
-        userid: socket.userid
-      },
-      msg: data.msg
-    };
-
-    // 메시지를 전송한 클라이언트를 제외한 모든 클라이언트에게 메시지를 전송한다
-    socket.broadcast.emit('chat', msg);
-
-    // 메시지를 전송한 클라이언트에게만 메시지를 전송한다
-    // socket.emit('s2c chat', msg);
-
-    // 접속된 모든 클라이언트에게 메시지를 전송한다
-    // io.emit('s2c chat', msg);
-
-    // 특정 클라이언트에게만 메시지를 전송한다
-    // io.to(id).emit('s2c chat', data);
+  
+  socket.on("msg", function (data) {
+    socketIo.to(idArr[data.recvId]).emit(
+              "msg", 
+              data.sendId + "님이 당신에게 귓말을 보냈습니다.\n" + data.msg);
   });
-
-  // force client disconnect from server
-  socket.on('forceDisconnect', function() {
-    socket.disconnect();
-  })
-
-  socket.on('disconnect', function() {
-    console.log('user disconnected: ' + socket.name);
-  });
-});
-
-server.listen(8888, function() {
-  console.log('Socket IO server listening on port 8888');
 });
