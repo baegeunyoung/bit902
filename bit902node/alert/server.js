25f3125b58814e97bde64cd26c8cee33a1a4754a
uruga00 <uruga00@gmail.com>
2017-05-06 17:05:29 +0900

    노드 ID 구분

--- bit902node/alert/server.js
+++ bit902node/alert/server.js
@@ -23,8 +23,9 @@ socketIo.on("connection", function (socket) {
 	// 입력한 아이디와 socket.id를 연결
 	idArr[loginId] = socket.id;
 	});
+
 	socket.on("msg", function (data) {
-		socketIo.to(idArr["msg".get(sendId)])
+		socketIo.to(idArr[data.sendId])
 		.emit("msg", "주문이 들어왔습니다.");
 	});
 });
