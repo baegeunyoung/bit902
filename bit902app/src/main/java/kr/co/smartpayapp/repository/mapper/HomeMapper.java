5823ba6a1b2bfd4da241dc45921c2c46072170af
flygil <gilsehoon@gmail.com>
2017-05-23 10:28:35 +0900

    homeMapper

--- bit902app/src/main/java/kr/co/smartpayapp/repository/mapper/HomeMapper.java
+++ bit902app/src/main/java/kr/co/smartpayapp/repository/mapper/HomeMapper.java
@@ -3,7 +3,6 @@ package kr.co.smartpayapp.repository.mapper;
 import java.util.List;
 
 import kr.co.smartpayapp.repository.vo.EventVO;
-import kr.co.smartpayapp.repository.vo.HomeEventInfoVO;
 import kr.co.smartpayapp.repository.vo.OrderHistoryVO;
 import kr.co.smartpayapp.repository.vo.StoreVO;
 
